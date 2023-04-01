6e48588720518788e72caab54160e55cf5eb4102
daianekr <daiane.kreitlow@gmail.com>
2023-04-01 14:37:08 -0300

    Create dataframe

--- main.py
+++ main.py
@@ -15,18 +15,3 @@ for linhas_tr in sp(carregaurl.content, "lxml")\
 
 df = pd.DataFrame(saida_colunas)
 print(df)
-# print(saida_colunas)
-
-
-# with open('todos_os_resultados.csv', 'w') as arquivo_com_resultados:
-
-#     arquivo_com_resultados.truncate(0)
-#     for resultado in list(saida_colunas):
-#         dez1 = int(resultado[17:19])
-#         dez2 = int(resultado[20:22])
-#         dez3 = int(resultado[23:25])
-#         dez4 = int(resultado[26:28])
-#         dez5 = int(resultado[29:31])
-#         dez6 = int(resultado[32:34])
-#         dezenas = f'{dez1},{dez2},{dez3},{dez4},{dez5},{dez6}'
-#         print(dezenas, file=arquivo_com_resultados)
