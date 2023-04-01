37b6fb312d67e96b54f045593439f3d48c7edcc1
daianekr <daiane.kreitlow@gmail.com>
2023-04-01 14:39:39 -0300

    Save Dataframe

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ for linhas_tr in sp(carregaurl.content, "lxml")\
     .findAll("div", class_="limpar_flutuacao"):
      saida_colunas.append(linhas_tr.previous_sibling)
 
+# print(saida_colunas)
+
+saida_colunas = [w.replace(' - ', '') for w in saida_colunas]
+
 df = pd.DataFrame(saida_colunas)
-print(df)
+
+df.rename(columns = {0: 'valores'}, inplace=True)
+df['Data'] = df['valores'].str[:10]
+
+
+df['Dezenas'] = df['valores'].str[11:28]
+
+df = df.drop(columns=['valores'])
+df.to_csv('dataframe_megasena.csv', index=True)
